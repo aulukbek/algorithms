284e41cb75d9533fbc15da8b9d3e57a836de66ad
aulukbek <ulukbek.attokurov@gmail.com>
2018-02-09 18:56:57 +0200

    Added list deletion methods

--- list_package/dlinked_list.py
+++ list_package/dlinked_list.py
@@ -29,18 +29,12 @@ class DLinkedList:
             self.__tail = node
             
     def __getPlace(self, pos):
-        current = self.__head
-        prev = self.__head
-        idx = 0
-        
-        while idx < pos and current.getNext():
-            prev = current
+        current = self.__head        
+        idx = 0        
+        while idx < pos and current.getNext():           
             current = current.getNext()
             idx += 1
-        return current 
-            
-        
-            
+        return current            
     
     def insertAtMiddle(self, value, pos):
         node = NodeDouble(value)
@@ -57,6 +51,40 @@ class DLinkedList:
             node.setPrev(current.getPrev())
             node.setNext(current)
             current.setPrev(node)
+            
+    def deleteAtStart(self):
+        if not self.__head:
+            raise ValueError("a list is empty")
+        else: 
+            tmp = self.__head            
+            self.__head = self.__head.getNext()
+            self.__head.setPrev(None)
+            tmp.setPrev(None)
+            tmp.setNext(None)
+    
+    def deleteAtEnd(self):
+        if not self.__tail:
+            raise ValueError("list is empty")
+        else:
+            tmp = self.__tail
+            self.__tail = self.__tail.getPrev()
+            self.__tail.setNext(None)
+            tmp.setNext(None)
+            tmp.setPrev(None)
+            
+    def deleteAtMid(self, pos):
+        if pos == 0  and not self.__head:
+            raise ValueError("a lsit is empty")
+        elif pos == 0 and self.__head:
+            self.deleteAtStart()
+        else: 
+            current = self.__getPlace(pos)
+            current.getPrev().setNext(current.getNext())
+            current.getNext().setPrev(current.getPrev())
+            current.setPrev(None)
+            current.setNext(None)
+            
+        
         
     def printList(self):
         current = self.__head
