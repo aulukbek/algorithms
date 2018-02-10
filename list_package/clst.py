68a023ff64958dbdecb3e14b8602ae0f11ce1afc
aulukbek <ulukbek.attokurov@gmail.com>
2018-02-10 16:56:57 +0200

    Added circular list delete operations

--- list_package/clst.py
+++ list_package/clst.py
@@ -36,7 +36,48 @@ class CircularList:
         else:
             tmp = Node(value)
             current = self.__getPlace(pos)
+            prev = current.get_pointer()
             current.set_pointer(tmp)
+            tmp.set_pointer(prev)
+    
+    def deleteAtStart(self):
+        if not self.__head:
+            raise ValueError("a list is empty")
+        else:
+            tmp = self.__head.get_pointer()
+            if tmp == self.__head:
+                self.__head = None
+            else:
+                last = self.__getLast()
+                self.__head.set_pointer(None)
+                self.__head = tmp
+                last.set_pointer(self.__head)
+    
+    def deleteAtEnd(self):
+        if not self.__head:
+            raise ValueError("a list is empty")
+        else:
+            current = self.__head
+            prev = current
+            while current.get_pointer() != self.__head:
+                prev = current
+                current = current.get_pointer()
+            if current == self.__head:
+                self.__head = None
+            else:
+                prev.set_pointer(self.__head)
+                current.set_pointer(None)
+                
+    def deleteAtMid(self, pos):
+        if pos == 0 and not self.__head:
+            raise ValueError("a list is empty")
+        elif pos == 0 and self.__head:
+            self.deleteAtStart()
+        else:
+            current = self.__getPlace(pos)
+            tmp = current.get_pointer()
+            current.set_pointer(tmp.get_pointer())
+            tmp.set_pointer(None)
             
     def __getLast(self):
         print "a function get last"
@@ -53,13 +94,15 @@ class CircularList:
         current = self.__head
         idx = 0
         prev = current
-        while current != self.__head and idx < pos:
+        while current.get_pointer() != self.__head and idx < pos - 1:
             prev = current
             current = current.get_pointer()
             idx += 1
         return prev
         
     def printCl(self):
+        if not self.__head:
+            raise ValueError(" a list is empty")
         print "a function is printCl"
         current = self.__head.get_pointer()
         print "head value is", self.__head.get_value()
